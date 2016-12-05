b3099e0d81f32310f691ec2b2cae1d8ce6c0e1ae
Jan-Christopher Pien <101@janchristopherpien.de>
2016-12-05 19:39:01 +0100

    JsonGenerator flush wieder rausgenommen

--- flinkydust-server/src/main/java/de/hu/flinkydust/server/rest/endpoint/DataPointRestEndpoint.java
+++ flinkydust-server/src/main/java/de/hu/flinkydust/server/rest/endpoint/DataPointRestEndpoint.java
@@ -66,7 +66,6 @@ public abstract class DataPointRestEndpoint {
                 }
             });
             jsonGenerator.writeEndObject();
-            jsonGenerator.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
