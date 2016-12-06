5b2fa81e96e9602e4cdc56b99f1155e4a59768d9
Jan-Christopher Pien <101@janchristopherpien.de>
2016-12-06 14:45:18 +0100

    POM update und FileUpload

--- flinkydust-server/src/main/java/de/hu/flinkydust/server/rest/endpoint/LoadTestDataEndpoint.java
+++ flinkydust-server/src/main/java/de/hu/flinkydust/server/rest/endpoint/LoadTestDataEndpoint.java
@@ -1,16 +1,16 @@
 package de.hu.flinkydust.server.rest.endpoint;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import de.hu.flinkydust.data.DataPoint;
 import de.hu.flinkydust.data.StreamDataSource;
 import de.hu.flinkydust.server.rest.datastore.DataStore;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.StreamingOutput;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by Jan-Christopher on 04.12.2016.
@@ -18,6 +18,11 @@ import java.io.IOException;
 @Path("/data")
 public class LoadTestDataEndpoint extends DataPointRestEndpoint {
 
+    /**
+     * Lädt die Testdaten ein.
+     * @return
+     *         Reponse-Objekt
+     */
     @GET
     @Path("/loadTest")
     @Produces(MediaType.APPLICATION_JSON)
@@ -28,22 +33,30 @@ public class LoadTestDataEndpoint extends DataPointRestEndpoint {
             return createErrorResponse(e.getMessage());
         }
 
-        return Response.ok().entity((StreamingOutput) (stream) -> {
-            JsonGenerator jsonGenerator = new ObjectMapper().getFactory().createGenerator(stream);
-            jsonGenerator.writeStartObject();
-            jsonGenerator.writeFieldName("status");
-            jsonGenerator.writeString("ok");
-            jsonGenerator.writeEndObject();
-
-            jsonGenerator.flush();
-            jsonGenerator.close();
-        }).type(MediaType.APPLICATION_JSON).build();
+        return createOkResponse();
     }
 
-//    @POST
-//    @Path("/load/file")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    @Consumes(MediaType.MULTIPART_FORM_DATA)
-//    public Response uploadFile(@)
+    /**
+     * Lädt eine Datei aus einem InputStream ein
+     * @param fileInputStream
+     *          InputStream
+     * @param fileMetaData
+     *          MetaData
+     * @return
+     *          Response
+     */
+    @POST
+    @Path("/load/file")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadFile(@FormDataParam("file") InputStream fileInputStream, @FormDataParam("file")FormDataContentDisposition fileMetaData) {
+        try {
+            DataStore.getInstance().putDataSource(DataPoint.class, StreamDataSource.parseFile(fileInputStream));
+        } catch (IOException e) {
+            return createErrorResponse(e.getMessage());
+        }
+
+        return createOkResponse();
+    }
 
 }
