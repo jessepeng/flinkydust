c1dd5b182b0b09f5a3d376c3b231a435402bbbe7
winnephew <ssjandi@web.de>
2017-02-08 01:46:24 +0100

    standard port

--- prototype/startup.sh
+++ prototype/startup.sh
@@ -1,9 +1,11 @@
 echo "Usage: startup.sh <port>"
 echo
 
+port=$1
 if [ -z "$1" ]
 	then
-	echo "Open your browser and go to http://localhost/"
+	port=8080
+	echo "Open your browser and go to http://localhost:8080/"
 else
 	echo "Open your browser and go to http://localhost:$1/"
 fi
@@ -12,4 +14,4 @@ echo
 echo "Starting server..."
 echo
 
-java -Xmx1024m -classpath "flinkydust-server-3.0.war:../lib/*;" de.hu.flinkydust.server.main.Server "$@"
+java -Xmx1024m -classpath "flinkydust-server-3.0.war:../lib/*" de.hu.flinkydust.server.main.Server "$port"
