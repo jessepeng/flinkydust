3cb06215102eced745ad69563d5428b26da08bfa
winnephew <ssjandi@web.de>
2017-02-09 13:29:42 +0100

    fixed scatter + clusters small/large

--- flinkydust-server/src/main/resources/web/js/plotly-scatter.js
+++ flinkydust-server/src/main/resources/web/js/plotly-scatter.js
@@ -22,6 +22,7 @@ function turnOffFinishListener() {
 }
 
 function refreshScatterplot() {
+    loadData(2);
     var chart = $('#chart');
     chart.empty();
     var timeline = $('#timeline');
@@ -582,9 +583,11 @@ function refreshClusters(){
     var restlink = '';
     // Create REST URL
     if(grains == 32){
+        loadData(32);
         restlink = '/rest/projection/MasterTime/GrainSize0_25/GrainSize0_28/GrainSize0_30/GrainSize0_35/GrainSize0_40/GrainSize0_45/GrainSize0_50/GrainSize0_58/GrainSize0_65/GrainSize0_70/GrainSize0_80/GrainSize1_0/GrainSize1_3/GrainSize1_6/GrainSize2_0/GrainSize2_5/GrainSize3_0/GrainSize3_5/GrainSize4_0/GrainSize5_0/GrainSize6_5/GrainSize7_5/GrainSize8_0/GrainSize10_0/GrainSize12_5/GrainSize15_0/GrainSize17_5/GrainSize20_0/GrainSize25_0/GrainSize30_0/GrainSize32_0/cluster/window/' + hours + '/';
-    }elseif( gains == 2){
-    var restlink = '/rest/projection/MasterTime/Small/Large/cluster/window/' + hours + '/';
+    }else if( grains == 2){
+        loadData(2);
+        restlink = '/rest/projection/MasterTime/Small/Large/cluster/window/' + hours + '/';
     }
 
 
@@ -911,13 +914,23 @@ function showTimeline(id){
 }
 
 
+function loadData(classes){
+    var restlink;
+    if(classes == 32){
+        restlink = "/rest/data/loadTestClasses";
+    }else{
+        restlink = "/rest/data/loadTest"
+    }
 
-$(document).ready(function () {
     $("body").append('<div class="loading-initial"><i class="fa fa-refresh fa-spin fa-5x"></i><span>Bitte warten, Daten werden geladen...</span></div>');
-    $.ajax("/rest/data/loadTestClasses", {
+
+    $.ajax(restlink, {
+        "async": false,
         "success": function() {
             $(".loading-initial").hide();
         }
     });
-//    setTimeout(refreshScatterplot,2500);
-});
+
+
+}
+
